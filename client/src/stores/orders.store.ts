032b289456f9124c0d456472fb1d77fe73f1ffeb
Valiantsin <valicser@yandex.ru>
2019-05-19 11:33:55 +0300

    fix: orders flow

--- client/src/stores/orders.store.ts
+++ client/src/stores/orders.store.ts
@@ -3,84 +3,89 @@ import { app } from 'firebase.config';
 import uuidv4 from 'uuid/v4';
 
 export class OrdersStore {
-    @observable public Orders?: any[];
-    @observable public ordersQuery?: firebase.firestore.QueryDocumentSnapshot[];
-    @observable public isLoading = false;
-    @observable public isAddInProgress = false;
+  @observable public Orders?: any[];
+  @observable public ordersQuery?: firebase.firestore.QueryDocumentSnapshot[];
+  @observable public isLoading = false;
+  @observable public isAddInProgress = false;
 
-    private usersCollection = app.firestore().collection('users');
-    private ordersCollection = app.firestore().collection('orders');
+  private usersCollection = app.firestore().collection('users');
+  private ordersCollection = app.firestore().collection('orders');
 
-    @action public addRow = async (order, phoneNumber) => {
-        this.isAddInProgress = true;
-        const newOrder = {
-            number: uuidv4(),
-            phoneNumber: phoneNumber,
-            executor: null,
-            order: {
-                name: order.requestName.value,
-                date: order.preferredTime,
-                services: [{
-                    count: 1,
-                    name: order.requestName.value,
-                    price: order.requestName.cost,
-                }],
-                status: 'Рассматривается',
-            },
-        };
-
-        await this.ordersCollection.add(newOrder)
-        runInAction(() => {
-            this.isAddInProgress = false;
-            setTimeout(() => { this.findExecuter(newOrder) }, 10000);
-        });
+  @action public addRow = async (order, phoneNumber) => {
+    this.isAddInProgress = true;
+    const newOrder = {
+      number: uuidv4(),
+      phoneNumber: phoneNumber,
+      executor: null,
+      order: {
+        name: order.requestName.value,
+        date: order.preferredTime,
+        services: [{
+          count: 1,
+          name: order.requestName.value,
+          price: order.requestName.cost,
+        }],
+        status: 'Рассматривается',
+      },
     };
 
-    @action public findExecuter = async (order) => {
-        const editOrder = this.ordersQuery!.find(o => o.data().number === order.number);
+    await this.ordersCollection.add(newOrder)
+    runInAction(() => {
+      this.isAddInProgress = false;
+      setTimeout(() => { this.findExecuter(newOrder) }, 10000);
+    });
+  };
 
-        if (editOrder) {
-            const executor = (await this.getExecuterPhineName()).id
-            console.log('DDD', executor);
-            await this.ordersCollection.doc(editOrder.id).update({
-                order: {
-                    ...order.data().order,
-                    executor: executor,
-                    status: 'Принята',
-                }
-            });
+  @action public findExecuter = async (order) => {
+    const snapshot = await this.ordersCollection
+      .where('phoneNumber', '==', order.phoneNumber)
+      .get();
+
+    const editOrder = snapshot.docs.find(o => o.data().number === order.number);
+
+    if (editOrder) {
+      const executor = (await this.getExecuterPhineName());
+      console.log('DDD', executor);
+      await this.ordersCollection.doc(editOrder.id).update({
+        order: {
+          ...editOrder.data().order,
+          status: 'Принята',
+        },
+        executor,
+      });
+    }
+  }
+
+  public getExecuterPhineName = async () => {
+    const usersSnapshot = await this.usersCollection.where('role', '==', 'employee').get();
+
+    const users: any[] = [];
+    usersSnapshot.forEach(userSnapshot => users.push(userSnapshot.id));
+    return users[0];
+  }
+
+  @action public load = async (phoneNumber) => {
+    this.isLoading = true;
+    const snapshot = await this.ordersCollection
+      .where('phoneNumber', '==', phoneNumber)
+      .get()
+
+    this.ordersQuery = snapshot.docs;
+
+    this.Orders = snapshot.docs.map(d => d.data())
+    runInAction(() => this.isLoading = false);
+  }
+
+  @action public updatePaymentStatus = async (number: number) => {
+    const order = this.ordersQuery!.find(order => order.data().number === number);
+
+    if (order) {
+      await this.ordersCollection.doc(order.id).update({
+        order: {
+          ...order.data().order,
+          status: 'Оплачено'
         }
+      });
     }
-
-    public getExecuterPhineName = async () => {
-        const usersSnapshot = await this.usersCollection.get();
-        const users = usersSnapshot.docs
-            .map(d => d.data()).filter((user) => user.role === 'employee');
-        return users[0];
-    }
-
-    @action public load = async (phoneNumber) => {
-        this.isLoading = true;
-        const snapshot = await this.ordersCollection
-            .where('phoneNumber', '==', phoneNumber)
-            .get()
-
-        this.ordersQuery = snapshot.docs;
-
-        this.Orders = snapshot.docs.map(d => d.data())
-        runInAction(() => this.isLoading = false);
-    }
-
-    @action public updatePaymentStatus = async (number: number) => {
-        const order = this.ordersQuery!.find(order => order.data().number === number);
-
-        if (order) {
-            await this.ordersCollection.doc(order.id).update({
-                order: {
-                    ...order.data().order,
-                    status: 'Оплачено'
-                }
-            });
-        }
-    }
+  }
 }
